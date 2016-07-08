96fb068973e41e70743588b23f79db1a2040bd1e
Vladyslav Hryhorenko <grigorenkovlad1993@gmail.com>
2016-07-08 23:44:02 +0300

    Initial commit

---
+++ WeatherApplication/WeatherApplication/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApplication.Services;
+
+namespace WeatherApplication.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private WeatherService service;
+        // GET: Weather
+        public WeatherController()
+        {
+            service = new WeatherService();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetWeather(string city,int term)
+        {
+            if (city == "")
+                RedirectToAction("Index", "WeatherController");
+            var result = service.GetWeatherForecast(city);
+            ViewBag.days = term;
+            return View(result);
+        }
+
+    }
+}
